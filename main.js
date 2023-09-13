7f65b9db39eb6bc2b2c6552f23728b870fa8d55c
johanp0308 <111256873+johanp0308@users.noreply.github.com>
2023-09-13 18:22:05 -0500

    feat: :beers: Se añadio codigo javascript
    En main js se añadio una funcion de saludar
    
    @johanp0308

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+
+
 console.log("Aqui tambien Somos los mejores");
 
 var hola = "Cristina la loca"
 
 console.log(hola);
+
+function hola(nombre){
+	console.log(`Hola ${nombre} ¿que tal?, ¿como te va?`);
+}
